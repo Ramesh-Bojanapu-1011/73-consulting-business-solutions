05b7f9c61b1886b544fb3ae15f178d9324ef1d21
Ramesh Bojanapu <84425972+Ramesh-Bojanapu-1011@users.noreply.github.com>
2025-10-22 09:32:26 +0530

    Initialize Next.js project with basic setup
    Add initial Next.js app structure including configuration, pages, components, styles, and assets. Includes .gitignore, README, TypeScript config, and theme support. Sets up Tailwind CSS and required dependencies.

---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SiteFooter = (props: Props) => {
+  return (
+    <div>SiteFooter</div>
+  )
+}
+
+export default SiteFooter
---
+++ src/components/SiteHeadder.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SiteHeadder = (props: Props) => {
+  return <div>SiteHeadder</div>;
+};
+
+export default SiteHeadder;
